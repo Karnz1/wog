fa7a3114bfa95bb1d26971af443f5c94bf64beba
Michael, Ramot <mich.ramot@gmail.com>
2023-10-05 18:39:41 +0300

    completed add_score function

--- score.py
+++ score.py
@@ -2,10 +2,18 @@ import Utils
 import os
 
 def add_score(difficulty):
-    with open("Scores.txt", "w+") as f:
-        
-
-
+    points_of_winning = (difficulty * 3) + 5
+    score_to_add = 0
+    if os.path.isfile("Scores.txt"):
+        if os.path.getsize('Scores.txt') > 0:
+            with open('Scores.txt', 'r') as f:
+                score_to_add += int(f.readline())
+                score_to_add += points_of_winning
+    else:
+        score_to_add += points_of_winning
+    with open('Scores.txt', 'w') as f:
+        print(score_to_add)
+        f.write(str(score_to_add))
 
 
 add_score(1)
